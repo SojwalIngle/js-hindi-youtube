f39371c6f2c54ce174972baea3ec9cfea300b27f
Sojwal Ingle <92205227+SojwalIngle@users.noreply.github.com>
2023-08-03 18:13:59 +0000

    new changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,19 +1,19 @@
 let score = "33"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valInNumber = Number(score)
 
-console.log(typeof valInNumber);
+// console.log(typeof valInNumber);
 
 let score1 = "33abc"
 let valInNumber1 = Number(score1)
-console.log(valInNumber1);
+// console.log(valInNumber1);
 
 
 let temp = null
 let valInNumber2 = Number(temp)
-console.log(valInNumber2);
+// console.log(valInNumber2);
 
 
 //"33" => 33
@@ -24,7 +24,7 @@ console.log(valInNumber2);
 
 let temp1 = undefined
 let valInNumber3 = Number(temp1)
-console.log(valInNumber3);
+// console.log(valInNumber3);
 
 
 
@@ -35,4 +35,44 @@ console.log(valInNumber3);
 
  let somNumber = 33
  let stringNumber = String(somNumber);
- console.log(typeof stringNumber);
+//  console.log(typeof stringNumber);
+
+
+
+ //***********************************************operations**************** */
+
+ let value = 3
+ let negValue = - value
+//  console.log(negValue);  // -3
+
+
+// console.log(2+2);
+// console.log(2**2);
+// console.log(2%3)
+
+
+let str1 = "hello"
+let str2 = " sojwal"
+
+let str3 = str1 + str2
+console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+// console.log(1 + 2 * 2 % 3); //2
+
+// console.log(+true); //1
+// console.log(+""); //0
+
+
+let num1 , num2 , num3;
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+
