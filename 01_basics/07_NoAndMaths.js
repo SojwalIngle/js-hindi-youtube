ff49bcac85883525e4644602bfadc565a51f4dc3
Sojwal Ingle <92205227+SojwalIngle@users.noreply.github.com>
2023-08-05 09:54:51 +0000

    Math and Number

---
+++ 01_basics/07_NoAndMaths.js
@@ -0,0 +1,60 @@
+
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(typeof balance);
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));  //manages decimal
+
+
+const otheerNumber = 123.8966
+
+// console.log(otheerNumber.toPrecision(3));
+
+const hund = 1000000
+
+// console.log(hund.toLocaleString());  //1,000,000
+// console.log(hund.toLocaleString('en-IN')); //10,00,000
+
+// console.log(Number.MAX_VALUE);
+
+
+
+
+// ++++++++++++++++++++++ MAths ++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+
+
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.2));  //top value
+// console.log(Math.floor(4.2)); //lower value
+// console.log(Math.min(5,6,7,8,9));
+
+// console.log(Math.max(5,6,7,8,9));
+
+console.log(Math.random()); // it gives 0 -1 values
+console.log(Math.random() * 10 + 1) //values will br start from 1 avoid zero
+// multiply by 10 will shift . to left and to avoid zero add 1
+
+
+
+console.log((Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) 
+
+
+
+
+
