92d6734572baf7a912108a49a4fdf165ddadcf99
Sojwal Ingle <92205227+SojwalIngle@users.noreply.github.com>
2023-08-11 10:24:05 +0000

    Control flow

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,55 @@
+// const temp = 41; 
+
+// if(temp === 40){
+//     console.log("temprature less than 50");
+// }
+// else{
+//     console.log("temp is greter than 50");
+// }
+
+
+
+
+// const score = 200;
+
+// if(score > 100){
+//     let power = "fly"
+// }
+// console.log(`USer power ${power}`);
+
+
+// const balance = 1000
+
+// if(balance > 500) console.log("yes");
+
+// const balance = 1000
+
+// if(balance < 500){
+//     console.log("less than ");
+// }
+// else if(balance < 750){
+//     console.log("less than 750");
+// }
+// else if(balance < 900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+// }
+
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInWithGoogle = false;
+const loggedInFromEmail = true;
+
+if(userLoggedIn && debitCard){
+    console.log("allow to buy");
+}
+
+if(loggedInFromEmail || loggedInFromEmail){
+    console.log("user logged in allow to buy");
+}
+
+
+
+
---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,41 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+        
+    case "march":
+        console.log("march");
+        break;
+    
+    default:
+        console.log("default case");
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
